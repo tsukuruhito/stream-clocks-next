065954738d24ff468f01a2f879fda038585aa387
tsukuruhito <ts.createlink@gmail.com>
2022-07-31 16:07:58 +0900

    add neon input

--- components/Control.tsx
+++ components/Control.tsx
@@ -1,5 +1,5 @@
 import home from "../styles/scss/Home.module.scss";
-import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import { ChangeEvent, Dispatch, FormEvent, MouseEvent, SetStateAction, useState } from "react";
 
 type PropsType = {
   color: string;
@@ -36,6 +36,7 @@ const Control = (props: PropsType) => {
   const geometryArray = ["pattern1", "pattern2", "pattern3"];
   const apexArray = ["red", "blue", "green"];
   const retroArray = ["pattern1", "pattern2"];
+  const [open, setOpen] = useState(false);
 
   const onClickType = (e: ChangeEvent<HTMLInputElement>) => {
     setSelectedType(e.target.value);
@@ -43,8 +44,8 @@ const Control = (props: PropsType) => {
   const colorSelect = (e: ChangeEvent<HTMLInputElement>) => {
     setColor(e.target.value);
   };
-  const neonSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    setNeon(e.target.value);
+  const neonSelect = (e: MouseEvent<HTMLInputElement>) => {
+    setNeon(e.currentTarget.value);
   };
   const apexSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     setApex(e.target.value);
@@ -55,6 +56,9 @@ const Control = (props: PropsType) => {
   const retroSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     setRetro(e.target.value);
   };
+  const openMenu = () => {
+    setOpen(!open);
+  }
 
   return (
     <section className={home.control}>
@@ -72,28 +76,50 @@ const Control = (props: PropsType) => {
               <label htmlFor={item}>{item}</label>
               {item === "noframe" && (
                 <input
-                  type="color"
+                  type="text"
                   defaultValue={color}
                   style={{
                     marginLeft: "10px",
-                    width: "50px",
+                    width: "100px",
                   }}
                   onChange={(e) => colorSelect(e)}
                 />
               )}
               {item === "neon" && (
-                <select
-                  style={{ marginLeft: "10px" }}
-                  onChange={(e) => neonSelect(e)}
-                >
-                  {neonArray.map((item, index) => {
-                    return (
-                      <option key={index} value={item}>
-                        {item}
-                      </option>
-                    );
-                  })}
-                </select>
+                // <select
+                //   style={{ marginLeft: "10px" }}
+                //   onChange={(e) => neonSelect(e)}
+                // >
+                //   {neonArray.map((item, index) => {
+                //     return (
+                //       <option key={index} value={item}>
+                //         {item}
+                //       </option>
+                //     );
+                //   })}
+                // </select>
+                <div className="relative">
+                    <button type="button" onClick={openMenu}>select</button>
+                    <div className={open? "absolute z-10 right-0": "absolute z-10 right-0 hidden"} >
+                        <ul>
+                            {neonArray.map((item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <input
+                                            type="radio"
+                                            name="neon"
+                                            value={item}
+                                            id={item}
+                                            onClick={(e) => neonSelect(e)}
+                                        />
+                                        <label htmlFor={item}>{item}</label>
+                                    </li>
+                                );
+                            }
+                            )}
+                        </ul>
+                    </div>
+                </div>
               )}
               {item === "geometory" && (
                 <select
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/inline-script-id */
 import type { NextPage } from "next";
 import Head from "next/head";
-import { ChangeEvent, useState } from "react";
+import Script from "next/script";
+import { useState } from "react";
 import Control from "../components/Control";
 import Time from "../components/Time";
 import home from "../styles/scss/Home.module.scss";
@@ -17,21 +19,22 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Stream Clocks</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d) {
-                var config = {
-                  kitId: 'umc4qnh',
-                  scriptTimeout: 3000,
-                  async: true
-                },
-                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-              })(document);
-            `
-          }}
-        />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d) {
+              var config = {
+                kitId: 'umc4qnh',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            })(document);
+          `
+        }}
+      />
       <div className={home.row}>
         <div className={home.frame}>
           <Time
@@ -43,22 +46,7 @@ const Home: NextPage = () => {
             retro={retro}
           />
         </div>
-        <div className={home.inner}>
-          <h1 className={home.page_title}>配信用&nbsp;時計オーバーレイ</h1>
-          <p>配信で時刻を表示したい場合の素材としてご利用いただけます。</p>
-          <p>デザインパターンは今後拡充予定です。</p>
-          <p>
-            ご利用にあたって、ご意見、ご要望などはTwitter（
-            <a
-              href="https://twitter.com/ts_create_"
-              target="_blanc"
-              rel="noopener"
-            >
-              @ts_create_
-            </a>
-            ）のDMでお願いします。
-          </p>
-          <Control
+        <Control
             color={color}
             setSelectedType={setSelectedType}
             setColor={setColor}
@@ -67,7 +55,34 @@ const Home: NextPage = () => {
             setApex={setApex}
             setRetro={setRetro}
           />
-          <small>
+      </div>
+        <div className={home.inner}>
+          <h1 className="text-4xl inline-block mb-4">配信用&nbsp;時計オーバーレイ</h1>
+          <div className="text-base font-mono">
+            <p>配信で時刻を表示したい場合の素材としてご利用いただけます。</p>
+            <p>デザインパターンは今後拡充予定です。</p>
+            <p>
+              ご利用にあたって、ご意見、ご要望などはTwitter（
+              <a
+                href="https://twitter.com/ts_create_"
+                target="_blanc"
+                rel="noopener"
+              >
+                @ts_create_
+              </a>
+              ）のDMでお願いします。
+            </p>
+            <h2 className="text-xl mt-4 inline-block">使用方法</h2>
+            <ol className="list-inside list-decimal">
+              <li>OBSのソースからブラウザを選択</li>
+              <li>このサイトのURLをコピー&amp;ペースト</li>
+              <li>幅:1000、高さ:1000を指定</li>
+              <li>カスタムCSSに記載がない場合は以下をコピー&amp;ペースト<div>body&#123;background-color: rgba(0, 0, 0, 0);&#125;</div></li>
+            </ol>
+          </div>
+          
+        </div>
+          <small className="px-5 box-border">
             <p>
               当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。
             </p>
@@ -94,8 +109,6 @@ const Home: NextPage = () => {
               ページをご覧ください。
             </p>
           </small>
-        </div>
-      </div>
     </>
   );
 };
