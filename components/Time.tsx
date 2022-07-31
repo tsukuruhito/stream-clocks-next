56d7674fbea4aa55abb749fd728f522e2ca1f1d1
tsukuruhito <ts.createlink@gmail.com>
2022-07-31 14:41:13 +0900

    fix without select system

--- components/Time.tsx
+++ components/Time.tsx
@@ -1,46 +1,61 @@
 import { useState } from "react";
+import home from "../styles/scss/Home.module.scss";
 
-const Time = (props:any) => {
-    const [date, setDate] = useState(new Date().toLocaleDateString());
-    const [time, setTime] = useState("");
-    const [day, setDay] = useState(new Date().toLocaleString("en-US", { weekday: "long" }));
-
-    setInterval(()=>{
-        const datetime = new Date().toLocaleTimeString().slice(0, -3);
-        setDate(new Date().toLocaleDateString());
-        setDay(new Date().toLocaleString("en-US", { weekday: "long" }));
-        if(datetime.length===4){
-            setTime("0"+datetime);
-        }else{
-            setTime(datetime);
-        }
-    },1000);
-
-    const setClass = () => {
-        if(props.selectedType==="neon"){
-            return `${props.selectedType} ${props.neon}`
-        }else if(props.selectedType==="geometory"){
-            return `${props.selectedType} ${props.geometory}`
-        }else if(props.selectedType==="apex"){
-            return `${props.selectedType} ${props.apex}`
-        }else if(props.selectedType==="retro"){
-            return `${props.selectedType} ${props.retro}`
-        }else{
-            return `${props.selectedType}`
-        }
-    }
-    
-    return(
-        <div className={setClass()}>
-            <div className="date_body"
-                {...props.selectedType==="noframe" && {style:{color:props.color}}}
-            >
-                <div className="date_day">{day}</div>
-                <div className="date_date">{date}</div>
-                <div className="date_time">{time}</div>
-            </div>
-        </div>
-    )
+type PropsType = {
+    selectedType: string,
+    color: string,
+    neon: string,
+    geometory: string,
+    apex: string,
+    retro: string,
 }
+const Time = (props: PropsType) => {
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [time, setTime] = useState("");
+  const [day, setDay] = useState(
+    new Date().toLocaleString("en-US", { weekday: "long" })
+  );
+  const {selectedType, color, neon, geometory, apex, retro} = props;
+
+  setInterval(() => {
+    const datetime = new Date().toLocaleTimeString().slice(0, -3);
+    setDate(new Date().toLocaleDateString());
+    setDay(new Date().toLocaleString("en-US", { weekday: "long" }));
+    if (datetime.length === 4) {
+      setTime("0" + datetime);
+    } else {
+      setTime(datetime);
+    }
+  }, 1000);
+
+  const setClass = () => {
+    if (selectedType === "neon") {
+      return `${selectedType} ${neon}`;
+    } else if (selectedType === "geometory") {
+      return `${selectedType} ${geometory}`;
+    } else if (selectedType === "apex") {
+      return `${selectedType} ${apex}`;
+    } else if (selectedType === "retro") {
+      return `${selectedType} ${retro}`;
+    } else {
+      return `${selectedType}`;
+    }
+  };
+
+  return (
+    <div className={setClass()}>
+      <div
+        className="date_body"
+        {...(selectedType === "noframe" && {
+          style: { color: color },
+        })}
+      >
+        <div className="date_day">{day}</div>
+        <div className="date_date">{date}</div>
+        <div className="date_time">{time}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Time;
