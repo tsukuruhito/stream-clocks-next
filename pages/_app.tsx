bf4919bb5ed203bf9a1b1146e584afa0dedaec0a
tsukuruhito <ts.createlink@gmail.com>
2022-08-01 17:30:09 +0900

    add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "../styles/scss/Clock.scss";
 import Head from "next/head";
+import GoogleTagManager, { GoogleTagManagerId } from "../components/GoogleTagManager";
+import { googleTagManagerId } from "../gtm";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Stream Clocks</title>
       </Head>
+      <GoogleTagManager googleTagManagerId={googleTagManagerId as GoogleTagManagerId}/>
       <Component {...pageProps} />
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,17 @@ const Document = () => {
     <Html lang="ja">
       <Head />
       <body>
+        <noscript
+            dangerouslySetInnerHTML={{
+                __html: `
+                    <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-WBQP74M"
+                    height="0"
+                    width="0"
+                    style="display:none;visibility:hidden"/>
+                `,
+            }}
+        />
         <Main />
         <NextScript />
       </body>
