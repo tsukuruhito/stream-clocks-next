052e6d21008e651989226e992378d295ce7f6cad
tsukuruhito <ts.createlink@gmail.com>
2022-08-01 14:15:29 +0900

    minify false

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import { HexColorPicker } from "react-colorful";
+
 type PropsType = {
   color: string;
   setColor: Dispatch<SetStateAction<string>>;
