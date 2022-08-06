c571e0dde95bc5eeb3e1a194aabcbd45df910bad
tsukuruhito <ts.createlink@gmail.com>
2022-08-06 19:06:56 +0900

    add note

--- components/Note.tsx
+++ components/Note.tsx
@@ -5,6 +5,12 @@ const Note = () => {
       <ul className="mb-4 p-2">
         <li className="mb-2">
           <dl className="flex flex-row-reverse justify-end pb-2 border-b-[1px] border-primary border-opacity-60">
+            <dt>新パターンの追加＆表示の調整を行いました。</dt>
+            <dd className="mr-4">2022/08/06</dd>
+          </dl>
+        </li>
+        <li className="mb-2">
+          <dl className="flex flex-row-reverse justify-end pb-2 border-b-[1px] border-primary border-opacity-60">
             <dt>OBSの対話モードに対応しました。</dt>
             <dd className="mr-4">2022/08/01</dd>
           </dl>
